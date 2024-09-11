0c1e65f7aa647b064d786a0543866a8d62b58d5d
Digvjay <digvijay.kewale@gmail.com>
2024-09-11 21:40:59 +0530

    code modified expensetracker class added

--- Expense_tracker/Expence_tracker.py
+++ Expense_tracker/Expence_tracker.py
@@ -3,6 +3,30 @@ from tkinter import messagebox, ttk, simpledialog
 import json
 from datetime import datetime
 
+
+class ExpenseTrackerApp:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Expense Tracker And Budget Planner")
+        self.root.geometry()
+        self.expenses = []
+        self.categories = ['Rent','Food','Entertainment','Car','CreditCards']
+        self.monthly_budget = self.get_initial_budget()
+        self.create_widgets()
+    
+    def get_initial_budget(self):
+        while True:
+            budget = simpledialog.askfloat("Monthly Budget","Enter your monthly budget")
+            if budget not in None:
+                return budget
+            else:
+                if messagebox.askyesno("No Budget","You havent Enter your budget Do you want to exit?"):
+                    self.root.quit()
+                    return 0
+
+
+
+
 def create_widget(self):
     main_frame = ttk.Frame(self.root,padding="10")
     main_frame.grid(row=0,column=0,sticky='NEWS')
@@ -34,6 +58,34 @@ def create_widget(self):
     display_frame = ttk.LabelFrame(main_frame,text="Expense and Budget",padding='10')
     display_frame.grid(row=0,column=1,rowspan=2,sticky="NEWS",padx=5,pady=5)
 
+    self.expense_tree = ttk.Treeview(display_frame,columns=('Date','Category','Amount'),show='headings')
+    self.expense_tree.heading('Date',text='Date')
+    self.expense_tree.heading('Category',text='Category')
+    self.expense_tree.heading('Amount',text='Amount')
+    self.expense_tree.grid(row=0,column=0,sticky='NEWS')
+
+    scrollbar = Scrollbar(display_frame,orient=VERTICAL,command=self.expense_tree)
+    scrollbar.grid(row=0,column=0,sticky='W',pady=2)
+    self.expense_tree.configure(yscrollcommand=scrollbar.set)
+
+    self.budget_label = Label(display_frame,text=f"Montly Budget: {self.monthly_budget:.2f}")
+    self.budget_label.grid(row=1,column=0,sticky='NEWS',pady=2)
+
+    self.total_expense_label = Label(display_frame,text='Total Expense: 0.00')
+    self.total_expense_label.grid(row=2,column=0,sticky='W',pady=2)
+
+    self.remaining_budget_label = Label(display_frame,text=f"Remaining Budget: {self.montthly_budget:.2f}")
+    self.remaining_budget_label.grid(row=3,column=0,sticky='W',pady=2)
+
+    Button(display_frame,text='Save Data',command=self.save_data).grid(row=4,column=0,pady=10)
+
+    self.root.columnconfigure(0,weight=1)
+    self.root.roeconfigure(0,weight=1)
+    main_frame.columnconfigure(1,weight=1)
+    main_frame.rowconfigure(0,weight=1)
+    display_frame.columnconfigure(0,weight=1)
+    display_frame.rowconfigure(0,weight=1)
+
     
 
 
@@ -51,6 +103,6 @@ def add_category(self):
 
 if __name__ == "__main__":
     root = Tk()
-    #app = ExpenceTrackerApp(root)
-    root.title("Expence Tracker")
+    app = ExpenseTrackerApp(root)
+    
     root.mainloop()
