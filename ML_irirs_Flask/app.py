2556715ab656bab644326b56d7f255d11c39ad16
Digvjay <digvijay.kewale@gmail.com>
2024-11-14 22:48:47 +0530

    Completed

--- ML_irirs_Flask/app.py
+++ ML_irirs_Flask/app.py
@@ -1,1 +1,23 @@
-import flask
+from flask import Flask,render_template,request
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open('D:\TinkerForm\ML_irirs_Flask\iris.sav','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    sepal_length = float(request.form['sepal_length'])
+    sepal_width =float(request.form['sepal_width'])
+    petal_length = float(request.form['petal_length'])
+    petal_width =float( request.form['petal_width'])
+    result = model.predict([[sepal_length,sepal_width,petal_length,petal_width]])
+    return render_template('index.html',result=result)
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
